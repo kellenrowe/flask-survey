b962cbb20c73dceff64648cf31266041bbb78807
David Lee <davidlee980804@gmail.com>
2020-12-03 16:09:40 -0800

    finished step 9

--- app.py
+++ app.py
@@ -9,33 +9,44 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 # responses = []
+RESPONSES_KEY = "responses"
+
 
 @app.route("/")
-def start_survey():
+def describe_survey():
     """define title and inst. variables / render start survey html """
 
     title = survey.title
     instructions = survey.instructions
 
-    session["responses"] = []
-
-    # Empty previous answers
-    # responses.clear()
+    # Set a new cookie for each user
 
     return render_template("survey_start.html",
                            title=title,
                            instructions=instructions)
 
 
-@app.route("/questions/<int:q_num>", methods=["POST", "GET"])
+@app.route("/begin", methods=["POST"])
+def start_survey():
+    """ Upon survey start, show the first question """
+
+    session[RESPONSES_KEY] = []
+
+    return redirect("/questions/0")
+
+
+@app.route("/questions/<int:q_num>")
 def show_question(q_num):
     """ takes current question as param and renders html based on question """
 
-    num_quests_answered = len(session["responses"])
+    num_quests_answered = len(session[RESPONSES_KEY])
 
+    # Check if wrong question is requested or if all questions
+    # have been answered
     if num_quests_answered == len(survey.questions):
         return redirect("/completion")
     elif q_num != num_quests_answered:
+        flash("Invalid Question Requested")
         return redirect(f"/questions/{num_quests_answered}")
 
     question = survey.questions[q_num]
@@ -52,15 +63,19 @@ def record_answer():
     """
     answer = request.form.get("answer")
 
-    responses = session["responses"]
-    responses.append(answer)
-    session["responses"] = responses
+    # Get the question number
+    q_num = int(request.form.get("q_num"))
 
-    # Get the next question number
-    q_num = int(request.form.get("q_num")) + 1
+    if answer is None:
+        flash("Answer required")
+        return redirect(f"/questions/{q_num}")
+
+    responses = session[RESPONSES_KEY]
+    responses.append(answer)
+    session[RESPONSES_KEY] = responses
 
     if q_num < len(survey.questions):
-        return redirect(f"/questions/{q_num}")
+        return redirect(f"/questions/{q_num + 1}")
     else:
         return redirect("/completion")
 
@@ -70,5 +85,4 @@ def show_thank_you():
     """ When all the questions have been answered,
         show the completion page
     """
-    
     return render_template("completion.html")
